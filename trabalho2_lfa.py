a570179c9229740e06654eaacf3b263735631f14
João Gehlen <joaogehlen91@gmail.com>
2016-06-20 16:23:57 -0300

    passo 2 e 3 do first is comming

--- trabalho2_lfa.py
+++ trabalho2_lfa.py
@@ -9,11 +9,41 @@ GLC = [
 estados_e_firsts = {}
 firsts = []
 
-def atualiza_firsts(estado, estado_origem):
-	for i in estados_e_firsts[estado_origem]:
-		
+
+def segundo_passo(estado, estado_origem):
+	sem_epslon = [x for x in estados_e_firsts[estado_origem] if x != '&']
+	estados_e_firsts[estado] = estados_e_firsts[estado] + sem_epslon
+
+'''
+def terceiro_passo(estado, producao): #<S>, #<A><B>
+	print producao[0:3], estados_e_firsts[producao[0:3]]
 
 
+	n_terminal = False
+	if producao[3] == '<':
+		n_terminal = True
+	
+	#while n_terminal:
+		#if producao
+
+	#if producao[4] == '<':
+
+	#if '&' in estados_e_firsts[]
+'''
+
+def first(producao, i=0, j=3):
+	estado = producao[i:j]
+	if '&' in estados_e_firsts[estado]:
+		i += 3
+		j += 3
+		return '' + first(producao[i:])
+	else:
+		if # tem que testar se não é um terminal 
+		return estado
+
+
+
+''' preparando dicionario'''
 for regra in GLC:
 	for estado in regra[0:1]:
 		estados_e_firsts.update({estado: ''})
@@ -25,18 +55,26 @@ for regra in GLC:
 	for producao in regra[1:]:
 		if producao[0] != '<':
 			linha.append(producao[0])
-
 	estados_e_firsts.update({regra[0]: linha})
 
+
+''' sgundo passo do first '''
 for regra in GLC:
-	linha = []
 	for producao in regra[1:]:
 		if producao[0] == '<':
-			# atualizar a lista de item do estado
-			atualiza_firsts(regra[0] ,producao[0:3])
-			print ""
-			#linha.append(estados_e_firsts[producao[0:3]])
+			segundo_passo(regra[0], producao[0:3])
 
-	#estados_e_firsts.update({regra[0]: linha})
+'''
+for regra in GLC:
+	for producao in regra[1:]:
+		if producao[0] == '<':
+			terceiro_passo(regra[0], producao)
 
-print estados_e_firsts	
+'''
+
+print first('<C><A>w')
+
+
+for i in estados_e_firsts:
+	print(i),(estados_e_firsts[i])
+
